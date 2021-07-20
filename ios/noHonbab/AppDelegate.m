bcd5cac911ec46895092d89346f2b974c6d2e4f5
blissful-y0 <blissful.y0@gmail.com>
2021-07-20 17:17:05 +0900

    Update: Firebase module for iOS/Andriod added

--- ios/noHonbab/AppDelegate.m
+++ ios/noHonbab/AppDelegate.m
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -27,6 +27,12 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // Add me --- \/
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+  // Add me --- /\
+  // ...
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
